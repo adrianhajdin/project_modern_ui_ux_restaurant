d3127d0f4ebef790f89cb24d2070d75e1eaf7fb0
Yatharth <yatharth0903@gmail.com>
2023-04-15 15:17:20 +0530

    changed link for booking table

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       <div className="app__navbar-login">
         <a href="#login" className="p__opensans">Log In / Registration</a>
         <div />
-        <a href="/" className="p__opensans">Book Table</a>
+        <a href="#login" className="p__opensans">Book Table</a>
       </div>
       <div className="app__navbar-smallscreen">
         <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
