2e0c195294ec6fc412b86a86aaf92cb4a2aaeecc
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-08-09 12:15:47 +0100

    updated dependencies and module system to 2023

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./container/Header/Header";
+import AboutUs from "./container/AboutUs/AboutUs";
+import SpecialMenu from "./container/Menu/SpecialMenu";
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
-import { Navbar } from './components';
-import './App.css';
+import Chef from "./container/Chef/Chef";
+import Intro from "./container/Intro/Intro";
+import Laurels from "./container/Laurels/Laurels";
+import Gallery from "./container/Gallery/Gallery";
+import FindUs from "./container/Findus/FindUs";
+import Footer from "./container/Footer/Footer";
+
+// import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import "./App.css";
 
 const App = () => (
   <div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
